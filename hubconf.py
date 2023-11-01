9ee012efd29192356c5a8f67f61528c0b028f557
grez72 <grez72@gmail.com>
2023-11-01 13:17:43 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,14 @@
 import torch, torchvision
-import .datasets
+from contextlib import contextmanager
+
+@contextmanager
+def temporary_sys_path(path):
+    """Temporarily add a directory to sys.path."""
+    sys.path.insert(0, path)
+    yield
+    sys.path.pop(0)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 dependencies = ['torch', 'torchvision']
 
@@ -10,4 +19,6 @@ def hello():
   return say_hello
 
 def imagenette():
-  return _datasets.imagenette
+    with temporary_sys_path(current_dir):
+        import datasets
+    return datasets.imagenette
