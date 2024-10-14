a020b0d64d1a02eb9a19b53489a4d3ce546b27d6
grez72 <grez72@gmail.com>
2024-10-14 07:11:45 -0400

    Update auth.py

--- visionlab_datasets/auth.py
+++ visionlab_datasets/auth.py
@@ -2,7 +2,7 @@ import boto3
 import requests
 from s3_filestore import auth
     
-def get_signed_url(bucket_name, bucket_key, expires_in_seconds=3600, profile='wasabi'):
+def get_signed_url(bucket_name, bucket_key, expires_in_seconds=3600, profile=None):
     s3_client = auth.get_client_with_userdata(profile)
     signed_url = s3_client.generate_presigned_url('get_object', 
                                                   Params={'Bucket': bucket_name, 'Key': bucket_key},
